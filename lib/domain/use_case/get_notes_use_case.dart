634c4f104e5d02252f835a36e20c6b84798433de
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-20 06:00:07 +0900

    [노트 앱] use_case 적용

---
+++ lib/domain/use_case/get_notes_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:note_app/domain/model/note.dart';
+import 'package:note_app/domain/repository/note_repository.dart';
+
+class GetNotesUseCase {
+  final NoteRepository repository;
+
+  GetNotesUseCase(this.repository);
+  Future<List<Note>> call() async {
+    List<Note> notes = await repository.getNotes();
+    return notes;
+  }
+}
--- lib/presentation/notes/notes_view_model.dart
+++ lib/presentation/notes/notes_view_model.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/domain/model/note.dart';
-import 'package:note_app/domain/repository/note_repository.dart';
+import 'package:note_app/domain/use_case/add_note_use_case.dart';
+import 'package:note_app/domain/use_case/delete_note_use_case.dart';
+import 'package:note_app/domain/use_case/get_notes_use_case.dart';
+import 'package:note_app/domain/use_case/use_cases.dart';
 import 'package:note_app/presentation/notes/notes_event.dart';
 import 'package:note_app/presentation/notes/notes_state.dart';
 
 class NotesViewModel with ChangeNotifier {
-  final NoteRepository repository;
+  final UseCases useCases;
+
   NotesState _state = NotesState(notes: []);
   NotesState get state => _state;
 
   Note? _recentlyDeletedNote;
 
-  NotesViewModel(this.repository) {
+  NotesViewModel(this.useCases) {
     _loadNotes();
   }
 
@@ -24,7 +28,8 @@ class NotesViewModel with ChangeNotifier {
   }
 
   Future<void> _loadNotes() async {
-    List<Note> notes = await repository.getNotes();
+    List<Note> notes = await useCases.getNotes();
+
     _state = state.copyWith(
       notes: notes,
     );
@@ -32,7 +37,7 @@ class NotesViewModel with ChangeNotifier {
   }
 
   Future<void> _deleteNotes(Note note) async {
-    await repository.deleteNote(note);
+    await useCases.deleteNote(note);
     _recentlyDeletedNote = note;
 
     await _loadNotes();
@@ -40,7 +45,7 @@ class NotesViewModel with ChangeNotifier {
 
   Future<void> _restoreNote() async {
     if (_recentlyDeletedNote != null) {
-      await repository.insertNote(_recentlyDeletedNote!);
+      await useCases.addNote(_recentlyDeletedNote!);
       _recentlyDeletedNote = null;
       _loadNotes();
     }
