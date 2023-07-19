ef383965757996befcf206c5856f9812c92e2447
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-20 06:33:42 +0900

    [노트 앱] 정렬 기능 추가

--- lib/presentation/notes/notes_view_model.dart
+++ lib/presentation/notes/notes_view_model.dart
@@ -4,13 +4,18 @@ import 'package:note_app/domain/use_case/add_note_use_case.dart';
 import 'package:note_app/domain/use_case/delete_note_use_case.dart';
 import 'package:note_app/domain/use_case/get_notes_use_case.dart';
 import 'package:note_app/domain/use_case/use_cases.dart';
+import 'package:note_app/domain/util/note_order.dart';
+import 'package:note_app/domain/util/order_type.dart';
 import 'package:note_app/presentation/notes/notes_event.dart';
 import 'package:note_app/presentation/notes/notes_state.dart';
 
 class NotesViewModel with ChangeNotifier {
   final UseCases useCases;
 
-  NotesState _state = NotesState(notes: []);
+  NotesState _state = NotesState(
+    notes: [],
+    noteOrder: const NoteOrder.date(OrderType.descending()),
+  );
   NotesState get state => _state;
 
   Note? _recentlyDeletedNote;
