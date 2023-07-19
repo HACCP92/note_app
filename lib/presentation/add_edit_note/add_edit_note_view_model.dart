f460e0f834033a01ac7095d599a1649589e3418a
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-20 04:50:58 +0900

    [노트 앱] 노트를 추가 작성과 표시 기능을 추가

---
+++ lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:note_app/domain/model/note.dart';
+import 'package:note_app/domain/repository/note_repository.dart';
+import 'package:note_app/presentation/add_edit_note/add_edit_note_event.dart';
+import 'package:note_app/presentation/add_edit_note/add_edit_note_ui_event.dart';
+import 'package:note_app/ui/colors.dart';
+
+class AddEditNoteViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  int _color = roseBud.value;
+  int get color => _color;
+  final _eventController = StreamController<
+      AddEditNoteUiEvent>.broadcast(); //broadcast 적용으로 여러번 리슨을 할수있다
+
+  Stream<AddEditNoteUiEvent> get eventStream => _eventController.stream;
+
+  AddEditNoteViewModel(this.repository);
+
+  void onEvent(AddEditNoteEvent event) {
+    event.when(
+      changeColor: _changeColor,
+      saveNote: _saveNote,
+    );
+  }
+
+  Future<void> _changeColor(int color) async {
+    _color = color;
+    notifyListeners();
+  }
+
+  Future<void> _saveNote(int? id, String title, String content) async {
+    if (id == null) {
+      await repository.insertNote(
+        Note(
+            title: title,
+            content: content,
+            color: _color,
+            timestamp: DateTime.now().millisecondsSinceEpoch),
+      );
+    } else {
+      await repository.updateNote(
+        Note(
+            id: id,
+            title: title,
+            content: content,
+            color: _color,
+            timestamp: DateTime.now().millisecondsSinceEpoch),
+      );
+    }
+    _eventController.add(const AddEditNoteUiEvent.saveNote());
+  }
+}
--- lib/presentation/notes/notes_screen.dart
+++ lib/presentation/notes/notes_screen.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:note_app/domain/model/note.dart';
 import 'package:note_app/presentation/add_edit_note/add_edit_note_screen.dart';
 import 'package:note_app/presentation/notes/components/note_item.dart';
+import 'package:note_app/presentation/notes/notes_event.dart';
+import 'package:note_app/presentation/notes/notes_view_model.dart';
 import 'package:note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<NotesViewModel>();
+    final state = viewModel.state;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your note', style: TextStyle(fontSize: 30)),
@@ -21,35 +26,27 @@ class NotesScreen extends StatelessWidget {
         elevation: 0,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          bool? isSaved = await Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const AddEditNoteScreen()),
           );
+          if (isSaved != null && isSaved) {
+            viewModel.onEvent(const NotesEvent.loadNotes());
+          }
         },
         child: const Icon(Icons.add),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView(
-          children: [
-            NoteItem(
-              note: Note(
-                title: 'title 1',
-                content: 'content 1',
-                color: wisteria.value,
-                timestamp: 1,
-              ),
-            ),
-            NoteItem(
-              note: Note(
-                title: 'title 2',
-                content: 'content 2',
-                color: skyBlue.value,
-                timestamp: 2,
-              ),
-            ),
-          ],
+          children: state.notes
+              .map(
+                (note) => NoteItem(
+                  note: note,
+                ),
+              )
+              .toList(),
         ),
       ),
     );
