ed6df49ab0574bef0cc4cd0313de01ef8d78d1c3
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-19 22:40:23 +0900

    [노트 앱] nav push 적용

---
+++ lib/presentation/add_edit_note/add_edit_note_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddEditNoteScreen extends StatelessWidget {
+  const AddEditNoteScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
